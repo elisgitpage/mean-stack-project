73bf04ed2ced1515eafdee5b97e20e799f004dd5
elisgitpage <eabidor@gmail.com>
2023-01-15 15:52:04 -0500

    Added getPosts retrieve from backend

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,23 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("First use statement");
-  next();
-});
-
-app.use((req, res, next) => {
-  res.send("This is the response from express!");
+app.use("/api/posts", (req, res, next) => {
+  const posts = [
+    {
+      id: "dfjdkfsl123",
+      title: "First server side post",
+      content: "Post from the server",
+    },
+    {
+      id: "ekfretg8egn",
+      title: "Second server side post",
+      content: "Second post from the server",
+    },
+  ];
+  res.status(200).json({
+    message: "Posts fetched successfully",
+    posts: posts,
+  });
 });
 
 module.exports = app;
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,13 +1,25 @@
 import { Subject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 import { Post } from './posts.model';
+import { Injectable } from '@angular/core';
 
+@Injectable()
 export class PostsService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
 
+  constructor(private http: HttpClient) {}
+
   getPosts() {
-    return [...this.posts];
+    this.http
+      .get<{ message: string; posts: Post[] }>(
+        'http://localhost:3000/api/posts'
+      )
+      .subscribe((postData) => {
+        this.posts = postData.posts;
+        this.postsUpdated.next([...this.posts]);
+      });
   }
 
   getPostUpdateListener() {

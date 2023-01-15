6df049ca0c41af5bad504fab3ee01aab0ae8ec81
Eli Abidor <eabidor@gmail.com>
2023-01-15 16:35:18 -0500

    Added addPost send new post to backend

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -27,7 +27,11 @@ export class PostsService {
   }
 
   addPost(post: Post) {
-    this.posts.push(post);
-    this.postsUpdated.next([...this.posts]);
+    this.http.post<{message: string}>('http://localhost:3000/api/posts', post)
+    .subscribe((responseData) => {
+      console.log(responseData.message);
+      this.posts.push(post);
+      this.postsUpdated.next([...this.posts]);
+    });
   }
 }

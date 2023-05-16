47c2d655edc4a1fe432caa0061e535fddd65f2b9
Eli Abidor <eabidor@gmail.com>
2023-05-16 07:05:12 -0400

    Extract user and post api routes to separate files

---
+++ backend/controllers/posts.js
@@ -0,0 +1,118 @@
+const Post = require("../models/post");
+
+exports.createPost = (req, res, next) => {
+  const url = req.protocol + "://" + req.get("host");
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+    image: url + "/images/" + req.file.filename,
+    creator: req.userData.userId,
+  });
+  post
+    .save()
+    .then((createdPost) => {
+      res.status(201).json({
+        message: "Post added successfully",
+        post: {
+          ...createdPost,
+          id: createdPost._id,
+        },
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "Post creation failed!",
+      });
+    });
+};
+
+exports.getPost = (req, res, next) => {
+  Post.findById(req.params.id)
+    .then((post) => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({ message: "Post not found!" });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "Fetching post failed!",
+      });
+    });
+};
+
+exports.getPosts = (req, res, next) => {
+  const pageSize = req.query.pagesize;
+  const currentPage = req.query.page;
+  let fetchedPosts;
+  const postQuery = Post.find();
+  if (pageSize && currentPage) {
+    postQuery.skip(pageSize * (currentPage - 1)).limit(pageSize);
+  }
+  console.log(req.query);
+  postQuery
+    .then((documents) => {
+      fetchedPosts = documents;
+      return Post.count();
+    })
+    .then((count) => {
+      res.status(200).json({
+        message: "Posts fetched successfully",
+        posts: fetchedPosts,
+        maxPosts: count,
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "Fetching posts failed!",
+      });
+    });
+};
+
+exports.updatePost = (req, res, next) => {
+  console.log(req);
+  let image = req.body.image;
+  if (req.file) {
+    const url = req.protocol + "://" + req.get("host");
+    image = url + "/images/" + req.file.filename;
+  }
+  const post = new Post({
+    _id: req.body.id,
+    title: req.body.title,
+    content: req.body.content,
+    image: image,
+    creator: req.userData.userId,
+  });
+  Post.updateOne({ _id: req.params.id, creator: req.userData.userId }, post)
+    .then((result) => {
+      console.log(result);
+      if (result.modifiedCount > 0) {
+        res.status(200).json({ message: "Update successful!" });
+      } else {
+        res.status(401).json({ message: "Not authorized!" });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "Couldn't update post!",
+      });
+    });
+};
+
+exports.deletePost = (req, res, next) => {
+  Post.deleteOne({ _id: req.params.id, creator: req.userData.userId })
+    .then((result) => {
+      console.log(result);
+      if (result.deletedCount > 0) {
+        res.status(200).json({ message: "Post deleted!" });
+      } else {
+        res.status(401).json({ message: "Not authorized!" });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "Deleting post failed!",
+      });
+    });
+};
